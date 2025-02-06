4099590c450e9bbd9547661060403af246ec473a
Mikita <ghjhjr11@gmail.com>
2025-02-06 15:48:07 +0200

    Done

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+
+function getElementWidth(content, padding, border) {
+    content = Number.parseFloat(content);
+    padding = Number.parseFloat(padding);
+    border = Number.parseFloat(border);
+
+    return content + 2 * padding + 2 * border;
+}
